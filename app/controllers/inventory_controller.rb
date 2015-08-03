b6bea1fbc21b36a686f724be51cce1ec09f3fbc4
James Traver <jtraverjr@gmail.com>
2015-08-03 16:20:48 -0500

    some views

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,16 +1,23 @@
 class InventoryController < ApplicationController
-  def index
 
-    items = Item.all
+
+  def index
+    @items = Item.all
 
   end
 
   def create
+    @items = Item.all
+
   end
 
   def edit
+    @items = Item.all
+
   end
 
   def delete
+    @items = Item.all
+
   end
 end
